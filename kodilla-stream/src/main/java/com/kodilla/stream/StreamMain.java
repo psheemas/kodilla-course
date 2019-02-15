db4b0e27f17241b160f5347a13a34827a2136dd5
psheemas <kulakowski.przemyslaw@gmail.com>
2019-02-15 14:53:25 +0100

    Second

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.Executor;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.lambda.Processor;
@@ -28,5 +29,8 @@ public class StreamMain {
             poemBeautifier.beautify("decorate", decoratePoem -> decoratePoem.toUpperCase());
             poemBeautifier.beautify("decorate", decoratePoem -> decoratePoem.replaceAll(".(?!$)", "$0                    "));
             poemBeautifier.beautify("decorate", decoratePoem -> decoratePoem.replaceAll(".(?!$)", "$0 "));
+
+            System.out.println("Using Stream to generate even numbers from 1 to 20");
+            NumbersGenerator.generateEven(20);
         }
 }
