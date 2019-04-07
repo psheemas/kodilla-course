da09e288f1e743e1797bdf77c29243359f2bf301
psheemas <kulakowski.przemyslaw@gmail.com>
2019-04-07 13:01:22 +0200

    13.4 hope its ok now

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/food2door/foodDoorService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/food2door/foodDoorService.java
@@ -1,14 +1,8 @@
 package com.kodilla.good.patterns.food2door;
 
-public class foodDoorService {
+public class FoodDoorService {
     public static void main(String[] args) {
-        System.out.println("Hello in Frajsko");
-        System.out.println("Please choose Your Favorite Producer");
-        UserInput userInput = new UserInput();
-        ProducentsInterface selectedProducer = userInput.OptionsScreen();
-        System.out.println("Here is the List of available products for " + selectedProducer.companyName());
-        System.out.println("Please choose what You want to order and contact with us: " + selectedProducer.contact());
-        System.out.println(selectedProducer.orderList());
-        selectedProducer.process();
+        Food2Door food2Door = new Food2Door();
+        food2Door.food2Door();
     }
 }
